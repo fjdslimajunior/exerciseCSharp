666b6e6d119afecfd6310e5afd2accf1aadb007e
Lima Junior <fjdlimajunior@gmail.com>
2025-01-21 02:39:49 -0300

    Include Basics

---
+++ Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int recebedor = 10;
+            Console.WriteLine(recebedor);
+            Console.WriteLine("Um valor aleatório");
+        }
+
+
+    }
+}
