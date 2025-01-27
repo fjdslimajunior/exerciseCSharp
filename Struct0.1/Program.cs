01475b9d9487f3242f3446ef470da865dbcf60c8
Lima Junior <fjdlimajunior@gmail.com>
2025-01-26 21:28:19 -0300

    Entendendo o uso de struct, métodos e funções

---
+++ Struct0.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Struct01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando um produto
+            Product produto = new Product(100, "Notebook", 299.9f);
+
+            // Exibindo os dados do produto
+            Console.WriteLine("ID: " + produto.Id);
+            Console.WriteLine("Nome: " + produto.Name);
+            Console.WriteLine("Preço em BRL: R$" + produto.Price);
+
+            // Calculando o preço em dólares
+            double taxaDolar = 4.9; // Exemplo: taxa de câmbio é 4.9
+            double precoEmDolar = produto.PriceInDolar(taxaDolar);
+            Console.WriteLine("Preço em US: $" + precoEmDolar);
+        }
+    }
+    struct Product
+    {
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+        public int Id;
+        public double Price;
+        public string Name;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }
+        public double MelhorValor()
+        {
+
+        }
+
+    }
+}
